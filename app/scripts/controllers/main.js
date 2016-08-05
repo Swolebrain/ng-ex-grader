84f948e7b46e8bab47e9da6f89decfc9e76f864d
Swolebrain <thecodingteacher@gmail.com>
2016-08-05 19:39:34 -0400

    feat: yeoman boilerplate

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngExGraderApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ngExGraderApp
+ */
+angular.module('ngExGraderApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
